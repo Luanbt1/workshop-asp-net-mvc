8d7523d32b378fe1c67347767afa1e9ef082540e
Luan Barleze <Luanbt63@gmail.com>
2022-08-05 14:50:58 -0300

    MySQL adaptation and first migration

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
 
 namespace WebApplication1
@@ -29,7 +24,7 @@ namespace WebApplication1
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                options.CheckConsentNeeded = context => true;   
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
@@ -37,7 +32,8 @@ namespace WebApplication1
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebApplication1Context>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebApplication1Context")));
+                    options.UseMySql(Configuration.GetConnectionString("WebApplication1Context"), builder =>
+            builder.MigrationsAssembly("WebApplication1")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
